cca83b2e99122fbaac8686d4736a31fe243f3047
roryward2003 <rorywardmh@gmail.com>
2025-01-27 17:47:32 -0500

    Implemented baisc functions for drawing circles and snowmen

--- src/q1.java
+++ src/q1.java
@@ -10,17 +10,21 @@ public class q1 {
     public static int width=4096;
     public static int height=4096;
 
+    // My own added test params, which will be variable in future
+    public static int testRadius=300;
+    public static int testX=width/2;
+    public static int testY=height/2;
+    public static final int RED=0xffff0000;
+    public static final int WHITE=0xffffffff;
+    enum Orientation {up, down, left, right};
+    public static double scale = 0.66;
+
     public static void main(String[] args) {
         try {
-
             // once we know what size we want we can creat an empty image
             BufferedImage outputimage = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+            drawSnowman(outputimage, testX, testY, testRadius, Orientation.up, WHITE);
 
-            for(int i=0; i<width/5; i++) {
-                for(int j=0; j<height/5; j++) {
-                    outputimage.setRGB((2*width/5)+i, (2*width/5)+j, 0x3fff00ff);
-                }
-            }
             // ------------------------------------
             // Your code would go here
             
@@ -47,6 +51,65 @@ public class q1 {
         } catch (Exception e) {
             System.out.println("ERROR " +e);
             e.printStackTrace();
+        }
+    }
+    
+    public static void drawSnowman(BufferedImage img, int xc, int yc, int r0, Orientation o, int colour) {
+        int r1 = (int)(r0*scale);
+        int r2 = (int)(r1*scale);
+        drawCircle(img, xc, yc, r0, colour);
+        switch (o) {
+            case up:
+                drawCircle(img, xc, yc-(r0+r1), r1, colour);
+                drawCircle(img, xc, yc-(r0+(2*r1)+r2), r2, colour);
+                break;
+            case down:
+                drawCircle(img, xc, yc+(r0+r1), r1, colour);
+                drawCircle(img, xc, yc+(r0+(2*r1)+r2), r2, colour);
+                break;
+            case left:
+                drawCircle(img, xc-(r0+r1), yc, r1, colour);
+                drawCircle(img, xc-(r0+(2*r1)+r2), yc, r2, colour);
+                break;
+            case right:
+                drawCircle(img, xc+(r0+r1), yc, r1, colour);
+                drawCircle(img, xc+(r0+(2*r1)+r2), yc, r2, colour);
+                break;
+            default:
+                break;
+        }
+    }
+
+    // This function draws a circle of radius r with centre (xc,yc), using
+    // Bresenham's algorithm. The fucntion itself is adapted from an example
+    // I found on GeeksForGeeks.org
+    public static void drawCircle(BufferedImage img, int xc, int yc, int r, int colour){
+        int x = 0, y = r;
+        int d = 3 - 2 * r;
+
+        drawPixels(img, xc, yc, x, y, colour);
+        while (y >= x){
+            // update decision param and y coord
+            if (d > 0) {
+                y--; 
+                d = d + 4 * (x - y) + 10;
+            }
+            else {
+                d = d + 4 * x + 6;
+            }
+            x++;
+            drawPixels(img, xc, yc, x, y, colour);
+        }
+    }
+
+    public static void drawPixels(BufferedImage img, int xc, int yc, int x, int y, int colour){
+        for(int i=-x; i<=x; i++) {
+            img.setRGB(xc+i, yc+y, colour);
+            img.setRGB(xc+i, yc-y, colour);
+        }
+        for(int i=-y; i<=y; i++) {
+            img.setRGB(xc+i, yc+x, colour);
+            img.setRGB(xc+i, yc-x, colour);
         }
     }
 }
